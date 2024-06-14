ee601e5bb525063d5ba9f55a224c59d99ec71938
kilvers <jknew92@gmail.com>
2024-06-14 08:23:12 +0100

    training loop with CV for model selection.

--- model.py
+++ model.py
@@ -7,7 +7,9 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder, FunctionTransfo
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.cluster import KMeans
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV,train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 
 # Custom transformer to create new features
 class FeatureCreator_sqft(BaseEstimator, TransformerMixin):
@@ -77,14 +79,72 @@ preprocessor = ColumnTransformer(
     transformers=[
         ('num', numerical_pipeline, numerical_features),
         ('cat', categorical_pipeline, categorical_features)
-    ]
+    ],
+        remainder = 'drop'
 )
 
 df = pd.read_csv('data/kc_house_data_NaN.csv').drop(columns='Unnamed: 0').dropna() 
 
+                                                      
+# X_train = df.drop(columns='price')
+# y_train = df.price
+# pipe.fit(X = X_train,y =  y_train)
+# from sklearn.metrics import r2_score
+# r2_score(y_train, y_pred)
+LR = LinearRegression()
+RFR = RandomForestRegressor()
+XGB = XGBRegressor()
+parameters = [ 
+            {#baseline 
+                'regressor': [LR],
+                'regressor__fit_intercept': [True, False]
+            },
 
-pipe = Pipeline(  steps=[
-    ('feature_creation_sqft_living', FeatureCreator_sqft('sqft_living',
+            {
+                'regressor': [RFR],
+                'regressor__n_estimators': [100, 200, 500, 1000],
+                'regressor__max_depth': [None, 10, 20, 30, 40, 50],
+                'regressor__min_samples_split': [2, 5, 10],
+                'regressor__min_samples_leaf': [1, 2, 4, 10],
+                'regressor__max_features': ['sqrt', 'log2'],
+                'regressor__bootstrap': [True, False]
+            },
+
+            {
+                'regressor': [XGB],
+                'regressor__n_estimators': [100, 200, 500, 1000],
+                'regressor__max_depth': [3, 4, 5, 6, 7, 8, 10],
+                'regressor__learning_rate': [0.01, 0.05, 0.1, 0.2, 0.3],
+                'regressor__subsample': [0.6, 0.7, 0.8, 0.9, 1.0],
+                'regressor__colsample_bytree': [0.6, 0.7, 0.8, 0.9, 1.0],
+                'regressor__gamma': [0, 0.1, 0.2, 0.3, 0.4, 0.5],
+                'regressor__reg_alpha': [0, 0.01, 0.1, 1, 10],
+                'regressor__reg_lambda': [0.1, 1, 10, 100]
+            }
+        ]
+
+#evaluating multiple classifiers
+#based on pipeline parameters
+#-------------------------------
+
+X_train, X_test, y_train, y_test = train_test_split(
+    df.drop(columns='price'), df.price, test_size=0.20, random_state=42)
+
+result=[]
+
+for params in parameters:
+
+    #classifier
+    regressor = params['regressor'][0]
+    print('running' , regressor)
+    #getting arguments by
+    #popping out regressor
+    # print(params)
+    params.pop('regressor')
+    
+
+    #pipeline
+    steps=[('feature_creation_sqft_living', FeatureCreator_sqft('sqft_living',
                                                     'sqft_living15',
                                                     'change_sqft_living_flag')),
     ('feature_creation_sqft_lot', FeatureCreator_sqft('sqft_lot',
@@ -93,15 +153,73 @@ pipe = Pipeline(  steps=[
     ('feature_creation_age_of_property', FeatureCreator_age('yr_built','yr_renovated')),
     ('add_kmeans_feature', KMeansTransformer()),
     ('preprocessor', preprocessor),
-    ('clf', LinearRegression())
+    ('regressor', regressor)]
 
-                               
+    
+    #cross validation using
+    #Grid Search
+    grid = GridSearchCV(Pipeline(steps= steps),
+                        param_grid=params,
+                        cv=3,
+                        scoring= ['neg_mean_squared_error'],
+                        refit = 'neg_mean_squared_error',
+                        n_jobs=-1
+                             )
+    
+    grid.fit(X_train, y_train)
+    # print(X_train)
+    #storing result
+    result.append(
+        {
+            'grid': grid,
+            'regressor': grid.best_estimator_,
+            'best score': grid.best_score_,
+            'best params': grid.best_params_,
+            'cv': grid.cv
+        }
+    )
+    # print(result.grid, result.regressor, result.best_score,result.best_params)
+    print('finished ', regressor)
 
-                                                                   ])
+#sorting result by best score
+# result = sorted(result, key=itemgetter('best score'),reverse=True)
+
+#saving best classifier
+# grid = result[0]['grid']
+# joblib.dump(grid, 'classifier.pickle')
 
 
-X_train = df.drop(columns='price')
-y_train = df.price
-pipe.fit(X = X_train,y =  y_train)
-from sklearn.metrics import r2_score
-r2_score(y_train, y_pred)
+
+# # Define the parameter grid
+# param_grid = {
+#                 # 'regressor': [RFR],
+#                 'regressor__n_estimators': [100, 200, 500, 1000],
+#                 'regressor__max_depth': [None, 10, 20, 30, 40, 50],
+#                 'regressor__min_samples_split': [2, 5, 10],
+#                 'regressor__min_samples_leaf': [1, 2, 4, 10],
+#                 'regressor__max_features': ['auto', 'sqrt', 'log2'],
+#                 'regressor__bootstrap': [True, False]
+#             }
+
+# # Initialize the XGBRegressor
+# xgb = XGBRegressor()
+
+
+#     #pipeline
+# steps = [
+#     ('feature_creation_sqft_living', FeatureCreator_sqft('sqft_living',
+#                                                 'sqft_living15',
+#                                                 'change_sqft_living_flag')),
+# ('feature_creation_sqft_lot', FeatureCreator_sqft('sqft_lot',
+#                                                 'sqft_lot15',
+#                                                 'change_sqft_lot_flag' )),
+# ('feature_creation_age_of_property', FeatureCreator_age('yr_built','yr_renovated')),
+# ('add_kmeans_feature', KMeansTransformer()),
+# ('preprocessor', preprocessor),
+# ('regressor', RandomForestRegressor())
+# ]
+# # Initialize GridSearchCV
+# grid_search = GridSearchCV(Pipeline(steps=steps), param_grid=param_grid, cv=5, n_jobs=-1, verbose=2, scoring='neg_mean_squared_error')
+# # grid_search = GridSearchCV(estimator=xgb, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2, scoring='neg_mean_squared_error')
+# # Fit the GridSearchCV to find the best parameters
+# grid_search.fit(X_train, y_train)
